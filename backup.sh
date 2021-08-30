834e64d20186a1028ce2130604f35fdd606b1c1e
Sergei Eremenko <finalitik@gmail.com>
2021-08-30 10:30:59 +0300

    Make backup.sh POSIX compatible
    - Make environment variables compatible with vaultwarden env file
    - Remove upload backup file with rclone
    - Change .tar.xz suffix with .tar.gz to reduce CPU load

--- backup.sh
+++ backup.sh
@@ -1,25 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
 set -ex
 
 # Use the value of the corresponding environment variable, or the
 # default if none exists.
-: ${VAULTWARDEN_ROOT:="$(realpath "${0%/*}"/..)"}
-: ${SQLITE3:="/usr/bin/sqlite3"}
-: ${RCLONE:="/usr/local/bin/rclone"}
+: "${DATA_FOLDER:=/data}"
+: "${ATTACHMENTS_FOLDER:=${DATA_FOLDER}/attachments}"
+: "${DATABASE_URL:=${DATA_FOLDER}/db.sqlite3}}"
+: "${CONFIG_FILE:=${DATA_FOLDER}/config.json}"
+: "${RSA_KEY_FILENAME:=${DATA_FOLDER}/rsa_key}"
+: "${SENDS_FOLDER:=${DATA_FOLDER}/sends}"
 
-DATA_DIR="data"
-BACKUP_ROOT="${VAULTWARDEN_ROOT}/backup"
+: "${BACKUP_ROOT:=/backup}"
+
 BACKUP_DIR_NAME="vaultwarden-$(date '+%Y%m%d-%H%M')"
 BACKUP_DIR_PATH="${BACKUP_ROOT}/${BACKUP_DIR_NAME}"
 BACKUP_FILE_DIR="archives"
-BACKUP_FILE_NAME="${BACKUP_DIR_NAME}.tar.xz"
+BACKUP_FILE_NAME="${BACKUP_DIR_NAME}.tar.gz"
 BACKUP_FILE_PATH="${BACKUP_ROOT}/${BACKUP_FILE_DIR}/${BACKUP_FILE_NAME}"
-DB_FILE="db.sqlite3"
 
-source "${BACKUP_ROOT}"/backup.conf
-
-cd "${VAULTWARDEN_ROOT}"
+cd "${DATA_FOLDER}"
 mkdir -p "${BACKUP_DIR_PATH}"
 
 # Back up the database using the Online Backup API (https://www.sqlite.org/backup.html)
@@ -29,42 +29,43 @@ mkdir -p "${BACKUP_DIR_PATH}"
 # but to minimize the possibility of a failed backup, implement a retry mechanism here.
 max_tries=10
 tries=0
-until ${SQLITE3} "file:${DATA_DIR}/${DB_FILE}?mode=ro" ".backup '${BACKUP_DIR_PATH}/${DB_FILE}'"; do
-    if (( ++tries >= max_tries )); then
-	echo "Aborting after ${max_tries} failed backup attempts..."
-	exit 1
+until sqlite3 "file:${DATABASE_URL}?mode=ro" ".backup '${BACKUP_DIR_PATH}/${DATABASE_URL##*/}'"; do
+    tries=$((tries+1))
+    if [ "$tries" -ge "$max_tries" ]; then
+        echo "Aborting after ${max_tries} failed backup attempts..."
+        exit 1
     fi
     echo "Backup failed. Retry #${tries}..."
-    rm -f "${BACKUP_DIR_PATH}/${DB_FILE}"
+    rm -f "${BACKUP_DIR_PATH}/${DATABASE_URL##*/}"
     sleep 1
 done
 
-backup_files=()
-for f in attachments config.json rsa_key.der rsa_key.pem rsa_key.pub.der rsa_key.pub.pem sends; do
-    if [[ -e "${DATA_DIR}"/$f ]]; then
-        backup_files+=("${DATA_DIR}"/$f)
+for i in \
+    "$ATTACHMENTS_FOLDER" \
+    "$CONFIG_FILE" \
+    "$RSA_KEY_FILENAME.der" \
+    "$RSA_KEY_FILENAME.pem" \
+    "$RSA_KEY_FILENAME.pub.der" \
+    "$RSA_KEY_FILENAME.pub.pem" \
+    "$SENDS_FOLDER"
+do
+    if [ -e "${DATA_FOLDER}/$i" ]; then
+        cp -a "$DATA_FOLDER/$i" "${BACKUP_DIR_PATH}"
     fi
 done
-cp -a "${backup_files[@]}" "${BACKUP_DIR_PATH}"
-tar -cJf "${BACKUP_FILE_PATH}" -C "${BACKUP_ROOT}" "${BACKUP_DIR_NAME}"
+
+tar -caf "${BACKUP_FILE_PATH}" -C "${BACKUP_ROOT}" "${BACKUP_DIR_NAME}"
 rm -rf "${BACKUP_DIR_PATH}"
 md5sum "${BACKUP_FILE_PATH}"
 sha1sum "${BACKUP_FILE_PATH}"
 
-if [[ -n ${GPG_PASSPHRASE} ]]; then
+if [ -n "${GPG_PASSPHRASE}" ]; then
     # https://gnupg.org/documentation/manuals/gnupg/GPG-Esoteric-Options.html
     # Note: Add `--pinentry-mode loopback` if using GnuPG 2.1.
     printf '%s' "${GPG_PASSPHRASE}" |
-    gpg -c --cipher-algo "${GPG_CIPHER_ALGO}" --batch --passphrase-fd 0 "${BACKUP_FILE_PATH}"
-    BACKUP_FILE_NAME+=".gpg"
-    BACKUP_FILE_PATH+=".gpg"
+        gpg -c --cipher-algo "${GPG_CIPHER_ALGO:-AES128}" --batch --passphrase-fd 0 "${BACKUP_FILE_PATH}"
+    BACKUP_FILE_NAME="$BACKUP_FILE_NAME.gpg"
+    BACKUP_FILE_PATH="$BACKUP_FILE_PATH.gpg"
     md5sum "${BACKUP_FILE_PATH}"
     sha1sum "${BACKUP_FILE_PATH}"
 fi
-
-# Attempt uploading to all remotes, even if some fail.
-set +e
-
-for dest in "${RCLONE_DESTS[@]}"; do
-    ${RCLONE} -vv --no-check-dest copy "${BACKUP_FILE_PATH}" "${dest}"
-done
